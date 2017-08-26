1639f870642c8cbd239fc10c29ebf168ccb4bddb
bungcip <devnull@localhost>
2017-08-26 19:33:59 +0700

    convert JsonEx, TilingSprite, Utils, Weather, WindowLayer

---
+++ ts/rpg_core/Weather.ts
@@ -0,0 +1,219 @@
+//-----------------------------------------------------------------------------
+/**
+ * The weather effect which displays rain, storm, or snow.
+ *
+ * @class Weather
+ * @constructor
+ */
+class Weather extends PIXI.Container {
+
+    protected _width: number;
+    protected _height: number;
+    protected _sprites: any[];
+
+    protected type: string;
+    protected power: number;
+    protected origin: Point;
+
+    protected _rainBitmap : Bitmap;
+    protected _stormBitmap: Bitmap;
+    protected _snowBitmap: Bitmap;
+
+    protected _dimmerSprite: ScreenSprite;
+
+    
+    /// bungcip: viewport itu tidak ada? sementara didefinisikan dulu....
+    public viewport;
+    
+
+    constructor() {
+        super();
+
+        this._width = Graphics.width;
+        this._height = Graphics.height;
+        this._sprites = [];
+
+        this._createBitmaps();
+        this._createDimmer();
+
+        /**
+         * The type of the weather in ['none', 'rain', 'storm', 'snow'].
+         *
+         * @property type
+         * @type String
+         */
+        this.type = 'none';
+
+        /**
+         * The power of the weather in the range (0, 9).
+         *
+         * @property power
+         * @type Number
+         */
+        this.power = 0;
+
+        /**
+         * The origin point of the weather for scrolling.
+         *
+         * @property origin
+         * @type Point
+         */
+        this.origin = new Point();
+    };
+
+    /**
+     * Updates the weather for each frame.
+     *
+     * @method update
+     */
+    update() {
+        this._updateDimmer();
+        this._updateAllSprites();
+    };
+
+    /**
+     * @method _createBitmaps
+     * @private
+     */
+    protected _createBitmaps() {
+        this._rainBitmap = new Bitmap(1, 60);
+        this._rainBitmap.fillAll('white');
+        this._stormBitmap = new Bitmap(2, 100);
+        this._stormBitmap.fillAll('white');
+        this._snowBitmap = new Bitmap(9, 9);
+        this._snowBitmap.drawCircle(4, 4, 4, 'white');
+    };
+
+    /**
+     * @method _createDimmer
+     * @private
+     */
+    protected _createDimmer() {
+        this._dimmerSprite = new ScreenSprite();
+        this._dimmerSprite.setColor(80, 80, 80);
+        this.addChild(this._dimmerSprite);
+    };
+
+    /**
+     * @method _updateDimmer
+     * @private
+     */
+    protected _updateDimmer() {
+        this._dimmerSprite.opacity = Math.floor(this.power * 6);
+    };
+
+    /**
+     * @method _updateAllSprites
+     * @private
+     */
+    protected _updateAllSprites() {
+        var maxSprites = Math.floor(this.power * 10);
+        while (this._sprites.length < maxSprites) {
+            this._addSprite();
+        }
+        while (this._sprites.length > maxSprites) {
+            this._removeSprite();
+        }
+        this._sprites.forEach(function (sprite) {
+            this._updateSprite(sprite);
+            sprite.x = sprite.ax - this.origin.x;
+            sprite.y = sprite.ay - this.origin.y;
+        }, this);
+    };
+
+    /**
+     * @method _addSprite
+     * @private
+     */
+    protected _addSprite() {
+        /// bungcip: viewport itu tidak ada? sementara didefinisikan dulu....
+        var sprite = new Sprite(this.viewport);
+        sprite.opacity = 0;
+        this._sprites.push(sprite);
+        this.addChild(sprite);
+    };
+
+    /**
+     * @method _removeSprite
+     * @private
+     */
+    protected _removeSprite() {
+        this.removeChild(this._sprites.pop());
+    };
+
+    /**
+     * @method _updateSprite
+     * @param {Sprite} sprite
+     * @private
+     */
+    protected _updateSprite(sprite) {
+        switch (this.type) {
+            case 'rain':
+                this._updateRainSprite(sprite);
+                break;
+            case 'storm':
+                this._updateStormSprite(sprite);
+                break;
+            case 'snow':
+                this._updateSnowSprite(sprite);
+                break;
+        }
+        if (sprite.opacity < 40) {
+            this._rebornSprite(sprite);
+        }
+    };
+
+    /**
+     * @method _updateRainSprite
+     * @param {Sprite} sprite
+     * @private
+     */
+    protected _updateRainSprite(sprite) {
+        sprite.bitmap = this._rainBitmap;
+        sprite.rotation = Math.PI / 16;
+        sprite.ax -= 6 * Math.sin(sprite.rotation);
+        sprite.ay += 6 * Math.cos(sprite.rotation);
+        sprite.opacity -= 6;
+    };
+
+    /**
+     * @method _updateStormSprite
+     * @param {Sprite} sprite
+     * @private
+     */
+    protected _updateStormSprite(sprite) {
+        sprite.bitmap = this._stormBitmap;
+        sprite.rotation = Math.PI / 8;
+        sprite.ax -= 8 * Math.sin(sprite.rotation);
+        sprite.ay += 8 * Math.cos(sprite.rotation);
+        sprite.opacity -= 8;
+    };
+
+    /**
+     * @method _updateSnowSprite
+     * @param {Sprite} sprite
+     * @private
+     */
+    protected _updateSnowSprite(sprite) {
+        sprite.bitmap = this._snowBitmap;
+        sprite.rotation = Math.PI / 16;
+        sprite.ax -= 3 * Math.sin(sprite.rotation);
+        sprite.ay += 3 * Math.cos(sprite.rotation);
+        sprite.opacity -= 3;
+    };
+
+    /**
+     * @method _rebornSprite
+     * @param {Sprite} sprite
+     * @private
+     */
+    protected _rebornSprite(sprite) {
+        sprite.ax = Math.randomInt(Graphics.width + 100) - 100 + this.origin.x;
+        sprite.ay = Math.randomInt(Graphics.height + 200) - 200 + this.origin.y;
+        sprite.opacity = 160 + Math.randomInt(60);
+    };
+
+
+}
+
+
---
+++ ts/rpg_core/WindowLayer.ts
@@ -0,0 +1,304 @@
+//-----------------------------------------------------------------------------
+/**
+ * The layer which contains game windows.
+ *
+ * @class WindowLayer
+ * @constructor
+ */
+class WindowLayer extends PIXI.Container {
+    protected _width: number;
+    protected _height: number;
+    protected _tempCanvas: any;
+    protected _translationMatrix: number[];
+    protected _windowMask: PIXI.Graphics;
+    protected _windowRect: any;
+    protected _renderSprite: any;
+
+    constructor() {
+        super();
+
+        this._width = 0;
+        this._height = 0;
+        this._tempCanvas = null;
+        this._translationMatrix = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+    
+        this._windowMask = new PIXI.Graphics();
+        this._windowMask.beginFill(0xffffff, 1);
+        this._windowMask.drawRect(0, 0, 0, 0);
+        this._windowMask.endFill();
+        /// bungcip: jadi any agar tidak error
+        this._windowRect = (this._windowMask as any).graphicsData[0].shape;
+    
+        this._renderSprite = null;
+        this.filterArea = new PIXI.Rectangle();
+        this.filters = [WindowLayer.voidFilter];
+    
+        //temporary fix for memory leak bug
+        this.on('removed', this.onRemoveAsAChild);
+    };
+
+    onRemoveAsAChild() {
+        this.removeChildren();
+    }
+    
+    static voidFilter = new PIXI.filters.VoidFilter();
+    
+    /**
+     * The width of the window layer in pixels.
+     *
+     * @property width
+     * @type Number
+     */
+        get width() {
+            return this._width;
+        }
+        set width(value) {
+            this._width = value;
+        }
+    
+    /**
+     * The height of the window layer in pixels.
+     *
+     * @property height
+     * @type Number
+     */
+        get height() {
+            return this._height;
+        }
+        set height(value) {
+            this._height = value;
+        }
+    
+    /**
+     * Sets the x, y, width, and height all at once.
+     *
+     * @method move
+     * @param {Number} x The x coordinate of the window layer
+     * @param {Number} y The y coordinate of the window layer
+     * @param {Number} width The width of the window layer
+     * @param {Number} height The height of the window layer
+     */
+    move(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    };
+    
+    /**
+     * Updates the window layer for each frame.
+     *
+     * @method update
+     */
+    update() {
+        this.children.forEach(function(child) {
+            if (child['update']) {
+                /// bungcip: jadi any agar bisa dipanggil
+                (child as any).update();
+            }
+        });
+    };
+    
+    /**
+     * @method _renderCanvas
+     * @param {Object} renderSession
+     * @private
+     */
+    renderCanvas(renderer) {
+        if (!this.visible || !this.renderable) {
+            return;
+        }
+    
+        if (!this._tempCanvas) {
+            this._tempCanvas = document.createElement('canvas');
+        }
+    
+        this._tempCanvas.width = Graphics.width;
+        this._tempCanvas.height = Graphics.height;
+    
+        var realCanvasContext = renderer.context;
+        var context = this._tempCanvas.getContext('2d');
+    
+        context.save();
+        context.clearRect(0, 0, Graphics.width, Graphics.height);
+        context.beginPath();
+        context.rect(this.x, this.y, this.width, this.height);
+        context.closePath();
+        context.clip();
+    
+        renderer.context = context;
+    
+        for (var i = 0; i < this.children.length; i++) {
+            var child = this.children[i];
+            /// bungcip: ada any agar bisa dicompile
+            if ((child as any)._isWindow && child.visible && (child as any).openness > 0) {
+                this._canvasClearWindowRect(renderer, child);
+                context.save();
+                child.renderCanvas(renderer);
+                context.restore();
+            }
+        }
+    
+        context.restore();
+    
+        renderer.context = realCanvasContext;
+        renderer.context.setTransform(1, 0, 0, 1, 0, 0);
+        renderer.context.globalCompositeOperation = 'source-over';
+        renderer.context.globalAlpha = 1;
+        renderer.context.drawImage(this._tempCanvas, 0, 0);
+    
+        for (var j = 0; j < this.children.length; j++) {
+            /// bungcip: ada any agar bisa dicompile
+            if (!(this.children[j] as any)._isWindow) {
+                this.children[j].renderCanvas(renderer);
+            }
+        }
+    };
+    
+    /**
+     * @method _canvasClearWindowRect
+     * @param {Object} renderSession
+     * @param {Window} window
+     * @private
+     */
+    protected _canvasClearWindowRect(renderSession, window) {
+        var rx = this.x + window.x;
+        var ry = this.y + window.y + window.height / 2 * (1 - window._openness / 255);
+        var rw = window.width;
+        var rh = window.height * window._openness / 255;
+        renderSession.context.clearRect(rx, ry, rw, rh);
+    };
+    
+    /**
+     * @method _renderWebGL
+     * @param {Object} renderSession
+     * @private
+     */
+    renderWebGL(renderer) {
+        if (!this.visible || !this.renderable) {
+            return;
+        }
+    
+        if (this.children.length==0) {
+            return;
+        }
+    
+        renderer.flush();
+        this.filterArea.copy(this as any);
+        renderer.filterManager.pushFilter(this, this.filters);
+        renderer.currentRenderer.start();
+    
+        var shift = new PIXI.Point();
+        var rt = renderer._activeRenderTarget;
+        var projectionMatrix = rt.projectionMatrix;
+        shift.x = Math.round((projectionMatrix.tx + 1) / 2 * rt.sourceFrame.width);
+        shift.y = Math.round((projectionMatrix.ty + 1) / 2 * rt.sourceFrame.height);
+    
+        for (var i = 0; i < this.children.length; i++) {
+            var child = this.children[i];
+            /// bungcip: ada any agar bisa dicompile
+            if ((child as any)._isWindow && child.visible && (child as any).openness > 0) {            
+                this._maskWindow(child, shift);
+                renderer.maskManager.pushScissorMask(this, this._windowMask);
+                renderer.clear();
+                renderer.maskManager.popScissorMask();
+                renderer.currentRenderer.start();
+                child.renderWebGL(renderer);
+                renderer.currentRenderer.flush();
+            }
+        }
+    
+        renderer.flush();
+        renderer.filterManager.popFilter();
+        renderer.maskManager.popScissorMask();
+    
+        for (var j = 0; j < this.children.length; j++) {
+            /// bungcip: ada any agar bisa dicompile
+            if (!(this.children[j] as any)._isWindow) {
+                this.children[j].renderWebGL(renderer);
+            }
+        }
+    };
+    
+    /**
+     * @method _maskWindow
+     * @param {Window} window
+     * @private
+     */
+    protected _maskWindow(window, shift) {
+        this._windowMask['_currentBounds'] = null; // bungcip: edited to compile
+        this._windowMask['boundsDirty'] = true as any; // bungcip: edited to compile
+        var rect = this._windowRect;
+        rect.x = this.x + shift.x + window.x;
+        rect.y = this.x + shift.y + window.y + window.height / 2 * (1 - window._openness / 255);
+        rect.width = window.width;
+        rect.height = window.height * window._openness / 255;
+    };
+        
+}
+
+
+
+
+// The important members from Pixi.js
+
+/**
+ * The x coordinate of the window layer.
+ *
+ * @property x
+ * @type Number
+ */
+
+/**
+ * The y coordinate of the window layer.
+ *
+ * @property y
+ * @type Number
+ */
+
+/**
+ * [read-only] The array of children of the window layer.
+ *
+ * @property children
+ * @type Array
+ */
+
+/**
+ * [read-only] The object that contains the window layer.
+ *
+ * @property parent
+ * @type Object
+ */
+
+/**
+ * Adds a child to the container.
+ *
+ * @method addChild
+ * @param {Object} child The child to add
+ * @return {Object} The child that was added
+ */
+
+/**
+ * Adds a child to the container at a specified index.
+ *
+ * @method addChildAt
+ * @param {Object} child The child to add
+ * @param {Number} index The index to place the child in
+ * @return {Object} The child that was added
+ */
+
+/**
+ * Removes a child from the container.
+ *
+ * @method removeChild
+ * @param {Object} child The child to remove
+ * @return {Object} The child that was removed
+ */
+
+/**
+ * Removes a child from the specified index position.
+ *
+ * @method removeChildAt
+ * @param {Number} index The index to get the child from
+ * @return {Object} The child that was removed
+ */
