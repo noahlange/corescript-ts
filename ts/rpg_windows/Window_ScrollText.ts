f20fb88b534afe44e3c9ce75f8c9cf5204248659
bungcip <devnull@localhost>
2017-08-29 21:11:15 +0700

    convert Window_ScrollText

---
+++ ts/rpg_windows/Window_ScrollText.ts
@@ -0,0 +1,87 @@
+//-----------------------------------------------------------------------------
+// Window_ScrollText
+//
+// The window for displaying scrolling text. No frame is displayed, but it
+// is handled as a window for convenience.
+
+class Window_ScrollText extends Window_Base {
+    protected _text;
+    protected _allTextHeight;
+
+    constructor() {
+        var width = Graphics.boxWidth;
+        var height = Graphics.boxHeight;
+        super(0, 0, width, height);
+        this.opacity = 0;
+        this.hide();
+        this._text = '';
+        this._allTextHeight = 0;
+    };
+
+    update() {
+        super.update();
+        if ($gameMessage.scrollMode()) {
+            if (this._text) {
+                this.updateMessage();
+            }
+            if (!this._text && $gameMessage.hasText()) {
+                this.startMessage();
+            }
+        }
+    };
+
+    startMessage() {
+        this._text = $gameMessage.allText();
+        this.refresh();
+        this.show();
+    };
+
+    refresh() {
+        var textState = { index: 0 };
+        textState['text'] = this.convertEscapeCharacters(this._text);
+        this.resetFontSettings();
+        this._allTextHeight = this.calcTextHeight(textState, true);
+        this.createContents();
+        this.origin.y = -this.height;
+        this.drawTextEx(this._text, this.textPadding(), 1);
+    };
+
+    contentsHeight() {
+        return Math.max(this._allTextHeight, 1);
+    };
+
+    updateMessage() {
+        this.origin.y += this.scrollSpeed();
+        if (this.origin.y >= this.contents.height) {
+            this.terminateMessage();
+        }
+    };
+
+    scrollSpeed() {
+        var speed = $gameMessage.scrollSpeed() / 2;
+        if (this.isFastForward()) {
+            speed *= this.fastForwardRate();
+        }
+        return speed;
+    };
+
+    isFastForward() {
+        if ($gameMessage.scrollNoFast()) {
+            return false;
+        } else {
+            return (Input.isPressed('ok') || Input.isPressed('shift') ||
+                TouchInput.isPressed());
+        }
+    };
+
+    fastForwardRate() {
+        return 3;
+    };
+
+    terminateMessage() {
+        this._text = null;
+        $gameMessage.clear();
+        this.hide();
+    };
+
+}
